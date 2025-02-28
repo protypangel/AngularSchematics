0da6a3adf92e926674dbfcd8e6c01c9d6cc894af
protypangel <mascret@et.esiea.fr>
2025-02-28 07:39:47 +0100

    + Updating structure

--- route/spec/helpers/reporter.ts
+++
@@ -1,8 +0,0 @@
-import {SpecReporter, StacktraceOption} from 'jasmine-spec-reporter';
-
-jasmine.getEnv().clearReporters(); // Supprime le reporter par défaut
-jasmine.getEnv().addReporter(new SpecReporter({
-    spec: {
-        displayStacktrace: StacktraceOption.PRETTY // ou 'summary', 'raw', 'none' selon vos préférences
-    }
-}));
